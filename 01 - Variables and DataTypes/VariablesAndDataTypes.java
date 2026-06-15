7966febf86b82a2b6d2169d2046c56bd6ef4fddb
ShahulKhan <iamshahulkhan@gmail.com>
2026-06-15 20:33:51 +0530

    Started adding Variables

---
+++ 01 - Variables and DataTypes/VariablesAndDataTypes.java
@@ -0,0 +1,15 @@
+public class VariablesAndDataTypes {
+
+	public static void main(String[] args) {
+
+		// Creating a String variable
+		String name = "Shahul";
+		System.out.println(name);
+
+		// Creating an integer variable
+		int age = 24;
+		System.out.println(age);
+
+	}
+	
+}
